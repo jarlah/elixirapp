ade645cbe73f3910e9d8cae017226d9e3f261622
Jarl André (Private) <jarlah@protonmail.com>
2021-10-28 18:38:47 +0200

    comment

--- lib/mix/tasks/eve-online.get-object-names.ex
+++ lib/mix/tasks/eve-online.get-object-names.ex
@@ -22,6 +22,7 @@ defmodule Mix.Tasks.EveOnline.GetObjectNames do
         Mix.shell().info("Program start")
 
         response =
+          # Either monad! xD
           with {:ok, orders} <-
                  get_market_orders(datasource, region_id, order_type, @default_page),
                type_ids <- get_unique_type_ids(orders),
